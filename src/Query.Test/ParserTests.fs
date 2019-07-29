bd43a24eb2ab20da8ea932be0a9081420417fd97
Arthur Rump <hello@arthurrump.com>
2019-07-29 19:28:43 +0200

    Add FsCheck test for parser

---
+++ src/Query.Test/ParserTests.fs
@@ -0,0 +1,54 @@
+module Query.Test.ParserTests
+
+open Expecto
+open FsCheck
+open System
+
+open Query.Language
+
+type QueryGen() =
+    static let isIdentifier str = 
+        str |> String.IsNullOrEmpty |> not
+        && str |> String.forall (fun c -> (c > 'a' && c < 'z') || (c > 'A' && c < 'Z') || c = '.')
+    
+    static member Expression() =
+        let rec expr' = function
+            | 0 -> Gen.oneof [
+                    Arb.generate |> Gen.where isIdentifier |> Gen.map Field
+                    Arb.generate |> Gen.map Literal ]
+            | x -> Gen.oneof [
+                    Gen.zip3 (expr' (x/2)) Arb.generate (expr' (x/2)) |> Gen.map Comparison
+                    Gen.zip3 (expr' (x/2)) Arb.generate (expr' (x/2)) |> Gen.map BooleanExpression
+                    expr' (x/2) |> Gen.map Negation ] 
+        Gen.sized expr' |> Arb.fromGen
+
+    static member Operation() =
+        Gen.oneof [
+            Arb.generate |> Gen.map Where
+            Gen.nonEmptyListOf Arb.generate |> Gen.map Select
+        ] |> Arb.fromGen
+
+    static member Pipeline() =
+        gen { 
+            let! ds = Arb.generate |> Gen.where isIdentifier
+            let! ops = Arb.generate
+            return { DataSource = ds; Operations = ops }
+        } |> Arb.fromGen
+
+let config = 
+    { FsCheckConfig.defaultConfig with 
+        arbitrary = [ typeof<QueryGen> ] }
+
+let testProp name = testPropertyWithConfig config name
+
+[<Tests>]
+let tests = 
+    testList "Parser" [
+        testPropertyWithConfig config "Parse -> Pretty -> Parse" <| fun pipeline ->
+            let pretty = PrettyPrint.prettyPipeline Cs pipeline
+            match parse pretty with
+            | FParsec.CharParsers.Success (result, _, _) ->
+                Expect.equal result pipeline "Parsed equals input"
+            | FParsec.CharParsers.Failure (mes, _, _) ->
+                failtestf "Parsing failed: %s" mes
+    ]
---
+++ src/Query.Test/Runner.fs
@@ -0,0 +1,8 @@
+﻿module Query.Test.Runner
+
+open Expecto
+
+[<EntryPoint>]
+let main args =
+    let tests = testList "All" [ ParserTests.tests ]
+    runTestsWithArgs defaultConfig args tests
